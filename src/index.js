c2f224dc68e6c1615fddb263b640b42b21f6af34
Quentin J <quentinjoubert123@gmail.com>
2020-07-17 10:09:28 +0200

    Implemented link's event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./css/style.css";
+
+const main = document.querySelector("main");
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", (e) =>  {
+    removeAllChildren(main);
+    const para = document.createElement("p");
+    para.textContent = e.target.dataset.page;
+    main.appendChild(para);
+  })
+});
+
+function removeAllChildren(element) {
+  while(element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
