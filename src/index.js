1182bcc586eccd170ffa244583a215df1591d8b9
Quentin J <quentinjoubert123@gmail.com>
2020-07-17 10:32:57 +0200

    Created about  module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "./css/style.css";
+import about from "./modules/about";
 
 const main = document.querySelector("main");
 const links = document.querySelectorAll("nav a");
 
 links.forEach(link => {
   link.addEventListener("click", (e) =>  {
+    const requestedPage = e.target.dataset.page;
     removeAllChildren(main);
-    const para = document.createElement("p");
-    para.textContent = e.target.dataset.page;
-    main.appendChild(para);
+    showPage(requestedPage);
   })
 });
 
@@ -16,4 +16,11 @@ function removeAllChildren(element) {
   while(element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+function showPage(page) {
+  const content = 
+    page === "about" ? about : document.createElement("p");
+
+  main.appendChild(content);
 }
---
+++ src/modules/about.js
@@ -0,0 +1,23 @@
+export default (function() {
+
+  const content = document.createElement("div");
+  const mainHeading = getMainHeading();
+  const introPara = getIntroPara();
+
+  content.appendChild(mainHeading);
+  content.appendChild(introPara);
+
+  function getMainHeading() {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Motcha Moment";
+    return h1;
+  }
+
+  function getIntroPara() {
+    const p = document.createElement("p");
+    p.textContent = "Restorative drinks in a peaceful environment";
+    return p;
+  }
+
+  return content;
+})();
